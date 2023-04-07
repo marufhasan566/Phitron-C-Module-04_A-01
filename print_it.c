173415f8ef8b7b9895c9febfe834a1095d00b958
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-07 16:48:12 +0600

    print_it fixed

--- print_it.c
+++ print_it.c
@@ -4,6 +4,6 @@ int main()
     printf("Hello, world! I am learning C programming language. ^_^\n");
     printf("Programming is fun and challenging. /\\/\\/\\\n"); 
     printf("I want to give my 100%% dedication to learn!\t");
-    printf("I will succeed one day");
+    printf("I will succeed one day.");
     return 0;
 }
