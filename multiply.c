a997347ba5cf5c84a692af431c69af91db6ea111
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-07 15:49:54 +0600

    multiply added

---
+++ multiply.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    long int A,B;
+    scanf("%ld %ld",&A,&B);
+    long int multiplication = A*B;
+    printf("%ld",multiplication);
+    return 0;    
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+    // printf("Programming is fun and challenging. /\\/\\/\\\n"); 
+    // printf("I want to give my 100%% dedication to learn!\t");
+    // printf("I will succeed one day");
